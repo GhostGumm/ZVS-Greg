eba83d79e06b9cdc484de8f7aca3a477c5f8b59a
Grégory Houllier <gregory.houllier@gmail.com>
2016-11-10 09:37:26 +0100

    feat(user) Update user interface

--- src/app/services/api/user/user.interface.ts
+++ src/app/services/api/user/user.interface.ts
@@ -19,35 +19,7 @@ export class UserClass implements UserInterface {
   avatar = './assets/zetalk_logo.png'
   metadata = {}
 
-  constructor(
-    parameters: {
-      id: string
-      login: string
-
-      firstname?: string
-      lastname?: string
-      online?: boolean
-      avatar?: string
-      metadata?: any
-    }
-  ) {
-      this.id = parameters.id
-      this.login = parameters.login
-
-      if (parameters.firstname) {
-        this.firstname = parameters.firstname
-      }
-      if (parameters.lastname) {
-        this.lastname = parameters.lastname
-      }
-      if (parameters.online) {
-        this.online = parameters.online
-      }
-      if (parameters.avatar) {
-        this.avatar = parameters.avatar
-      }
-      if (parameters.metadata) {
-        this.metadata = parameters.metadata
-      }
+  constructor(parameters: UserInterface) {
+    Object.assign(this, parameters)
   }
 }
