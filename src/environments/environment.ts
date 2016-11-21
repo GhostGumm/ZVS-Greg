6b83ab5331fbaaa76da36c057ad94843aa6dd46f
Grégory Houllier <gregory.houllier@gmail.com>
2016-11-21 14:47:27 +0100

    fix(env) Update local env case

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,10 @@
-const ZETAPUSH_API_URL = 'http://localhost:10080/zbo/pub/business/'
-const ZETAPUSH_SANDBOX_ID = 'yZWGxtkN'
+const ZETAPUSH_SERVEUR_URL = 'http://hq.zpush.io'
+const ZETAPUSH_API_URL = `${ZETAPUSH_SERVEUR_URL}:9080/zbo/pub/business/`
+const ZETAPUSH_SANDBOX_ID = 'dq3eV2XI'
 // const ZETAPUSH_PROXY_URL =  `http://file.zpush.ovh/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
-const ZETAPUSH_PROXY_URL =  `http://localhost:10080/str/rest/deployed/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
+const ZETAPUSH_PROXY_URL =  `${ZETAPUSH_SERVEUR_URL}:9081/str/rest/deployed/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
 
 export const environment = {
-  production: false,
+  production: true,
   ZETAPUSH_API_URL, ZETAPUSH_PROXY_URL, ZETAPUSH_SANDBOX_ID
 }
