2332328ea7bf4ed3186fefcae21ec4472263d2a6
Grégory Houllier <gregory.houllier@gmail.com>
2016-11-21 14:20:12 +0100

    fix(env) Update deploy url

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,8 +1,8 @@
-const ZETAPUSH_SERVEUR_URL = 'http://localhost:10080'
-const ZETAPUSH_API_URL = `${ZETAPUSH_SERVEUR_URL}/zbo/pub/business/`
+const ZETAPUSH_SERVEUR_URL = 'http://localhost'
+const ZETAPUSH_API_URL = `${ZETAPUSH_SERVEUR_URL}:10080/zbo/pub/business/`
 const ZETAPUSH_SANDBOX_ID = 'yZWGxtkN'
 // const ZETAPUSH_PROXY_URL =  `http://file.zpush.ovh/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
-const ZETAPUSH_PROXY_URL =  `${ZETAPUSH_SERVEUR_URL}/str/rest/deployed/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
+const ZETAPUSH_PROXY_URL =  `${ZETAPUSH_SERVEUR_URL}:10081/str/rest/deployed/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
 
 export const environment = {
   production: false,
