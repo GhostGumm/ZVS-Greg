896c4c0707f04c0e0717ee0f65cbb60fadcb2001
Grégory Houllier <gregory.houllier@gmail.com>
2016-11-21 14:13:36 +0100

    fix(env) Replace demo by correct sandbox id

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -1,6 +1,6 @@
 const ZETAPUSH_SERVEUR_URL = 'http://hq.zpush.io:9080'
 const ZETAPUSH_API_URL = `${ZETAPUSH_SERVEUR_URL}/zbo/pub/business/`
-const ZETAPUSH_SANDBOX_ID = 'yZWGxtkN'
+const ZETAPUSH_SANDBOX_ID = 'Gfbi0sp8'
 // const ZETAPUSH_PROXY_URL =  `http://file.zpush.ovh/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
 const ZETAPUSH_PROXY_URL =  `${ZETAPUSH_SERVEUR_URL}/str/rest/deployed/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
 
