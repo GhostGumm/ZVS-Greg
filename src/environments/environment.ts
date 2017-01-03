456e71432ad594ad3f048bc09b7aec3c57667c8f
Grégory Houllier <gregory.houllier@gmail.com>
2017-01-03 14:53:47 +0100

    feat(env) Update default sandbox id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 const ZETAPUSH_SERVEUR_URL = 'http://hq.zpush.io'
 const ZETAPUSH_API_URL = `${ZETAPUSH_SERVEUR_URL}:9080/zbo/pub/business/`
-const ZETAPUSH_SANDBOX_ID = 'OWsGroaZ'
+const ZETAPUSH_SANDBOX_ID = 'W2OOXd1R'
 // const ZETAPUSH_PROXY_URL =  `http://file.zpush.ovh/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
 const ZETAPUSH_PROXY_URL =  `${ZETAPUSH_SERVEUR_URL}:9081/str/rest/deployed/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
 const ZETAPUSH_DELEGATING_TOKEN_KEY = 'ServicesAuthToken'
