51eba84f77fecbab4e220905e33f669205e50948
Grégory Houllier <gregory.houllier@gmail.com>
2017-01-17 11:04:42 +0100

    chore(clean) Remove useless component lifecycle event

--- src/app/root.component.ts
+++ src/app/root.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { Router } from '@angular/router'
 
 @Component({
@@ -13,19 +13,6 @@ import { Router } from '@angular/router'
   `],
   template: '<section class="Layout" zp-connection-status><router-outlet></router-outlet></section>'
 })
-export class RootComponent implements AfterViewInit, OnInit {
+export class RootComponent {
   constructor(private router: Router) {}
-  ngOnInit() {
-    console.debug('AppComponent::ngOnInit')
-  }
-  ngAfterViewInit() {
-    console.debug('AppComponent::ngAfterViewInit')
-    /* UI boot struck prevent loader fadeOut :(
-      let loader = <HTMLElement>document.querySelector('zp-loader')
-      loader.style.visibility='hidden'
-      setTimeout(() => loader.remove, 500)
-    */
-  }
 }
-
-
