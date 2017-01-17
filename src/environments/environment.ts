8f1a4a1ca6a9d4bc7aaf97bedd7cb5c482ff5235
Grégory Houllier <gregory.houllier@gmail.com>
2017-01-17 10:52:47 +0100

    chore(env) Update default env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 const ZETAPUSH_SERVEUR_URL = 'http://hq.zpush.io'
 const ZETAPUSH_API_URL = `${ZETAPUSH_SERVEUR_URL}:9080/zbo/pub/business/`
-const ZETAPUSH_SANDBOX_ID = 'q5zadDcP'
+const ZETAPUSH_SANDBOX_ID = 'dwQeLGcd'
 // const ZETAPUSH_PROXY_URL =  `http://file.zpush.ovh/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
 const ZETAPUSH_PROXY_URL =  `${ZETAPUSH_SERVEUR_URL}:9081/str/rest/deployed/${ZETAPUSH_SANDBOX_ID}/cnvrst_hdfs/`
 const ZETAPUSH_DELEGATING_TOKEN_KEY = 'ServicesAuthToken'
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './polyfills.ts'
+import './polyfills'
 import 'hammerjs'
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
